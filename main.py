ccb407889c40aa76030d6cf18b0773e94898b003
Sidivan <Sidivan@hotmail.com>
2025-11-24 18:09:16 -0600

    player can now move around

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
+from logger import log_state
+from player import Player
+import circleshape
+
 def main():
-	import pygame
-	from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-	from logger import log_state
+
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -9,18 +13,27 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	game_clock = pygame.time.Clock()
 	dt = 0
-
+	player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+	print("Updatable:", updatable)
+	print("Drawable:", drawable)
 	while True:
 		log_state()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		updatable.update(dt)
+		for drawing in drawable:
+			drawing.draw(screen)
+		
 		pygame.display.flip()
-		dt = game_clock.tick(60)
-		print(dt)
+		dt = game_clock.tick(60) / 1000
+		#print(dt)
             
       
 
